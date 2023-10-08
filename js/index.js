539f6b1cf3d63707c4a6c558f28991fff5d26434
b82a9a13 <85174237+b82a9a13@users.noreply.github.com>
2023-10-08 13:25:03 +0100

    0.23
    Change Log:
    - Automatic updating of the table when it is open and new data is added to the database
    - Validation added to index.js functions

--- js/index.js
+++ js/index.js
@@ -5,52 +5,54 @@ fetch("./json/headers.json")
         headers = json;
     })
 function table(type,number){
-    const errorText = document.getElementById('table_error');
-    errorText.style.display = 'none';
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "./inc/table.inc.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function(){
-        if(this.status == 200){
-            const response = JSON.parse(this.responseText);
-            if(response['error']){
-                errorText.innerText = response['error'];
-                errorText.style.display = 'block';
-            } else if(response['data']){
-                let buttons = document.querySelectorAll('.table-btn');
-                for(let i = 0; i < buttons.length; i++){
-                    if(buttons[i].innerText.includes('Refresh')){
-                        buttons[i].innerText = buttons[i].innerText.replace('Refresh','Show');
-                    }
-                }
-                let data = response['data'];
-                let newTable = `<h2 class='text-center'>${type} Table</h2><table id='current_table' tval='${type}' pval='${number}' class='table-striped' style='margin-left:auto;margin-right:auto;'><thead><tr>`;
-                for(let i = 0; i < headers[number].length; i++){
-                    newTable += '<th>'+headers[number][i].heading+'</th>';
-                }
-                newTable += "</tr></thead><tbody>";
-                for(let i = 0; i < data.length; i++){
-                    newTable += '<tr>';
-                    for(let y = 0; y < data[i].length; y++){
-                        if(headers[number][y].type === 'Currency'){
-                            newTable += '<td>£'+data[i][y]+'</td>';
-                        } else {
-                            newTable += '<td>'+data[i][y]+'</td>';
+    if((type == 'Balance' && number == 0) || (type == 'Accounting In' && number == 1) || (type == 'Accounting Out' && number == 2) || (type == 'Petty Cash ID' && number == 3) || (type == 'Petty Cash Type' && number == 4)){
+        const errorText = document.getElementById('table_error');
+        errorText.style.display = 'none';
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "./inc/table.inc.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function(){
+            if(this.status == 200){
+                const response = JSON.parse(this.responseText);
+                if(response['error']){
+                    errorText.innerText = response['error'];
+                    errorText.style.display = 'block';
+                } else if(response['data']){
+                    let buttons = document.querySelectorAll('.table-btn');
+                    for(let i = 0; i < buttons.length; i++){
+                        if(buttons[i].innerText.includes('Refresh')){
+                            buttons[i].innerText = buttons[i].innerText.replace('Refresh','Show');
                         }
                     }
-                    newTable += '</tr>';
+                    let data = response['data'];
+                    let newTable = `<h2 class='text-center'>${type} Table</h2><table id='current_table' tval='${type}' pval='${number}' class='table-striped' style='margin-left:auto;margin-right:auto;'><thead><tr>`;
+                    for(let i = 0; i < headers[number].length; i++){
+                        newTable += '<th>'+headers[number][i].heading+'</th>';
+                    }
+                    newTable += "</tr></thead><tbody>";
+                    for(let i = 0; i < data.length; i++){
+                        newTable += '<tr>';
+                        for(let y = 0; y < data[i].length; y++){
+                            if(headers[number][y].type === 'Currency'){
+                                newTable += '<td>£'+data[i][y]+'</td>';
+                            } else {
+                                newTable += '<td>'+data[i][y]+'</td>';
+                            }
+                        }
+                        newTable += '</tr>';
+                    }
+                    newTable += "</tbody></table>";
+                    document.getElementById('table_div').innerHTML = newTable;
+                    document.getElementById(type.toLowerCase()).innerText = document.getElementById(type.toLowerCase()).innerText.replace('Show','Refresh');
+                } else {
+                    errorText.innerText = 'Loading error';
+                    errorText.style.display = 'block';
                 }
-                newTable += "</tbody></table>";
-                document.getElementById('table_div').innerHTML = newTable;
-                document.getElementById(type.toLowerCase()).innerText = document.getElementById(type.toLowerCase()).innerText.replace('Show','Refresh');
-            } else {
-                errorText.innerText = 'Loading error';
-                errorText.style.display = 'block';
-            }
 
-        }   
+            }   
+        }
+        xhr.send(`type=${type}`);
     }
-    xhr.send(`type=${type}`);
 }
 function input(type){
     if(document.getElementById(type.toLowerCase()+" form").style.display == 'block'){
@@ -112,8 +114,7 @@ document.getElementById('add invoice form').addEventListener("submit", (e) => {
                         table(value1, value2);
                     }
                 }
-                update_stat('bank');
-                update_stat('petty');
+                update_stats();
             } else {
                 errorText.innerText = errorText.innerText.slice(0, -1)
                 errorText.style.display = 'block'
@@ -177,8 +178,7 @@ document.getElementById('add receipt form').addEventListener("submit", (e) => {
                         table(value1, value2);
                     }
                 }
-                update_stat('bank');
-                update_stat('petty');
+                update_stats();
             } else {
                 errorText.innerText = errorText.innerText.slice(0, -1)
                 errorText.style.display = 'block'
@@ -241,8 +241,7 @@ document.getElementById('add bank transaction form').addEventListener("submit", 
                         table(value1, value2);
                     }
                 }
-                update_stat('bank');
-                update_stat('petty');
+                update_stats();
             } else {
                 errorText.innerText = errorText.innerText.slice(0, -1)
                 errorText.style.display = 'block'
@@ -267,6 +266,11 @@ document.getElementById('logout_btn').addEventListener('click', ()=>{
     }
     xhr.send();
 })
+function update_stats(){
+    update_stat('bank');
+    update_stat('petty');
+    update_table();
+}
 function update_stat(type){
     if(type == 'bank' || type == 'petty'){
         const errorText = document.getElementById(`${type}_stat_error`);
@@ -294,5 +298,25 @@ function update_stat(type){
             }
         }
         xhr.send(`t=${type}`);
+    }
+}
+function update_table(){
+    const type = document.getElementById('current_table').getAttribute('tval');
+    switch(type){
+        case 'Balance':
+            table(type, 0);
+            break;
+        case 'Accounting In':
+            table(type, 1);
+            break;
+        case 'Accounting Out':
+            table(type, 2);
+            break;
+        case 'Petty Cash ID':
+            table(type, 3);
+            break;
+        case 'Petty Cash Type':
+            table(type, 4);
+            break;
     }
 }
